1f4cbadc934a90e1585fa9f62d6ca93a5fafdbf8
최준성 <147041315+yumXD@users.noreply.github.com>
2024-06-18 17:05:35 +0900

    Fix: 버그 수정
    통신에러, 캐릭터 미 표시

--- src/characters/Player.js
+++ src/characters/Player.js
@@ -2,91 +2,60 @@ import Phaser from "phaser";
 import Config from "../Config";
 
 export default class Player extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene) {
-        // 화면의 가운데에 player를 추가해줍니다.
-        // scene.add.existing : scene에 오브젝트를 추가
-        // scene.physics.add.existing : scene의 물리엔진에 오브젝트를 추가
-        super(scene, Config.width / 2, Config.height / 2, "playerIdle");
-        scene.add.existing(this);
-        scene.physics.add.existing(this);
+  constructor(
+    scene,
+    x = Config.width / 2,
+    y = Config.height / 2,
+    texture = "player"
+  ) {
+    super(scene, x, y, texture);
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
 
-        // scale 프로퍼티를 조절해 크기를 조절할 수 있습니다. (디폴트: 1)
-        this.scale = 0.4;
+    this.scale = 0.4;
+    this.setDepth(20);
 
-        // depth를 조절해 어떤 오브젝트가 앞에 오고 뒤에 올지 설정할 수 있습니다.
-        // CSS의 z-index와 비슷한 개념입니다. (디폴트: 0)
-        this.setDepth(20);
+    this.setBodySize(28, 32);
 
-        // 해당 오브젝트가 물리적으로 얼만큼의 면적을 차지할 지 설정하는 함수입니다.
-        // 디폴트로 이미지 사이즈로 설정되는데, 그러면 추후 몹을 추가했을 때 너무 잘 부딪히는 느낌이 드므로 원본 이미지보다 약간 작게 설정해주었습니다.
-        this.setBodySize(28, 32);
+    this.m_moving = false;
+    this.m_attacking = false;
+  }
 
+  move(vector) {
+    if (this.m_attacking) return;
+    let PLAYER_SPEED = 3;
+
+    this.x += vector[0] * PLAYER_SPEED;
+    this.y += vector[1] * PLAYER_SPEED;
+
+    if (vector[0] === -1) this.flipX = false;
+    else if (vector[0] === 1) this.flipX = true;
+
+    if (vector[0] !== 0 || vector[1] !== 0) {
+      if (!this.m_moving) {
+        this.play("player_anim");
+        this.m_moving = true;
+      }
+    } else {
+      if (this.m_moving) {
+        this.play("player_idle");
         this.m_moving = false;
-        this.m_attacking = false;
-
-        this.createAnimations(scene);
+      }
     }
 
-    createAnimations(scene) {
-        scene.anims.create({
-            key: 'player_anim',
-            frames: scene.anims.generateFrameNumbers("playerRun1"),
-            frameRate: 12,
-            repeat: -1
-        });
+    this.scene.socket.emit("playerMovement", {
+      x: this.scene.m_player.x,
+      y: this.scene.m_player.y,
+    });
+  }
 
-        scene.anims.create({
-            key: 'player_idle',
-            frames: scene.anims.generateFrameNumbers("playerIdle1"),
-            frameRate: 12,
-            repeat: -1
-        });
-
-        scene.anims.create({
-            key: 'attack',
-            frames: scene.anims.generateFrameNumbers("playerAttack1"),
-            frameRate: 12,
-            repeat: 0
-        });
-    }
-
-    move(vector) {
-        // console.log(vector);
-        if (this.m_attacking) return;
-        let PLAYER_SPEED = 3;
-
-        this.x += vector[0] * PLAYER_SPEED;
-        this.y += vector[1] * PLAYER_SPEED;
-
-        // 캐릭터 이미지 원본은 왼쪽을 바라보고 있습니다.
-        // flipX 프로퍼티는 boolean 값을 받아 x축 방향으로 뒤집혀있을지 아닐지를 설정합니다.
-        // player가 왼쪽으로 이동할 때는 flipX = false,
-        // player가 오른쪽쪽으로 이동할 때는 flipX = true로 설정해 적절한 방향을 바라보게 해 줍니다.
-        if (vector[0] === -1) this.flipX = false;
-        else if (vector[0] === 1) this.flipX = true;
-
-        if (vector[0] !== 0 || vector[1] !== 0) {
-            if (!this.m_moving) {
-                this.play('player_anim');
-                this.m_moving = true;
-            }
-        } else {
-            if (this.m_moving) {
-                this.play('player_idle');
-                this.m_moving = false;
-            }
-        }
-    }
-
-    attack() {
-        if (this.m_attacking) return; // 이미 공격 중이면 아무것도 하지 않음
-
-        this.m_attacking = true;
-        this.play('attack', true);
-
-        this.once('animationcomplete', () => {
-            this.m_attacking = false;
-            this.play('player_anim');
-        });
-    }
+  attack() {
+    // if (this.m_attacking) return; // 이미 공격 중이면 아무것도 하지 않음
+    // this.m_attacking = true;
+    // this.play("attack", true);
+    // this.once("animationcomplete", () => {
+    //   this.m_attacking = false;
+    //   this.play("player_anim");
+    // });
+  }
 }
--- src/scenes/PlayingScene.js
+++ src/scenes/PlayingScene.js
@@ -11,170 +11,172 @@ export default class PlayingScene extends Phaser.Scene {
     this.otherPlayers = {};
   }
 
-    create() {
-        this.socket = io("http://143.248.177.132:3000");
+  create() {
+    this.socket = io("http://143.248.177.132:3000");
 
-        // 서버와 연결된 후 자신의 소켓 아이디를 출력
-        this.socket.on("connect", () => {
-          console.log("My socket ID:", this.socket.id);
-        });
-    
-        this.socket.on("currentPlayers", (players) => {
-          Object.keys(players).forEach((id) => {
-            if (players[id].playerId != this.socket.id) {
-              this.addOtherPlayers(players[id]);
-            }
-          });
-        });
-    
-        this.socket.on("newPlayer", (playerInfo) => {
-          this.addOtherPlayers(playerInfo);
-        });
-    
-        this.socket.on("playerMoved", (playerInfo) => {
-          if (this.otherPlayers[playerInfo.playerId]) {
-            const otherPlayer = this.otherPlayers[playerInfo.playerId];
-    
-            // 이전 위치와 현재 위치를 비교하여 방향을 설정합니다.
-            if (playerInfo.x > otherPlayer.x) {
-              otherPlayer.flipX = true;
-            } else if (playerInfo.x < otherPlayer.x) {
-              otherPlayer.flipX = false;
-            }
-    
-            // 이전 위치를 업데이트합니다.
-            otherPlayer.previousX = otherPlayer.x;
-            otherPlayer.previousY = otherPlayer.y;
-    
-            otherPlayer.setPosition(playerInfo.x, playerInfo.y);
-          }
-        });
-    
-        this.socket.on("disconnected", (playerId) => {
-          if (this.otherPlayers[playerId]) {
-            this.otherPlayers[playerId].destroy();
-            delete this.otherPlayers[playerId];
-          }
-        });
-        
-        // 사용할 sound들을 추가해놓는 부분입니다.
-        // load는 전역적으로 어떤 scene에서든 asset을 사용할 수 있도록 load 해주는 것이고,
-        // add는 해당 scene에서 사용할 수 있도록 scene의 멤버 변수로 추가할 때 사용하는 것입니다.
-        this.sound.pauseOnBlur = false;
-        this.m_beamSound = this.sound.add("audio_beam");
-        this.m_scratchSound = this.sound.add("audio_scratch");
-        this.m_hitMobSound = this.sound.add("audio_hitMob");
-        this.m_growlSound = this.sound.add("audio_growl");
-        this.m_explosionSound = this.sound.add("audio_explosion");
-        this.m_expUpSound = this.sound.add("audio_expUp");
-        this.m_hurtSound = this.sound.add("audio_hurt");
-        this.m_nextLevelSound = this.sound.add("audio_nextLevel");
-        this.m_gameOverSound = this.sound.add("audio_gameOver");
-        this.m_gameClearSound = this.sound.add("audio_gameClear");
-        this.m_pauseInSound = this.sound.add("audio_pauseIn");
-        this.m_pauseOutSound = this.sound.add("audio_pauseOut");
+    // 서버와 연결된 후 자신의 소켓 아이디를 출력
+    this.socket.on("connect", () => {
+      console.log("My socket ID:", this.socket.id);
+    });
 
-        // player를 m_player라는 멤버 변수로 추가합니다.
-        this.m_player = new Player(this);
-        this.m_player.play('player_idle');
+    this.socket.on("currentPlayers", (players) => {
+      Object.keys(players).forEach((id) => {
+        if (players[id].playerId !== this.socket.id) {
+          this.addOtherPlayers(players[id]);
+        }
+      });
+    });
 
-        this.cameras.main.startFollow(this.m_player);
+    this.socket.on("newPlayer", (playerInfo) => {
+      this.addOtherPlayers(playerInfo);
+    });
 
-        setBackground(this, "background1");
+    this.socket.on("playerMoved", (playerInfo) => {
+      if (this.otherPlayers[playerInfo.playerId]) {
+        const otherPlayer = this.otherPlayers[playerInfo.playerId];
 
-        this.m_cursorKeys = this.input.keyboard.createCursorKeys();
-        this.m_attackKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
-    }
-
-    update() {
-        this.movePlayerManager();
-
-        if (Phaser.Input.Keyboard.JustDown(this.m_attackKey)) {
-            this.createClaw();
-            this.m_player.attack();
+        // 이전 위치와 현재 위치를 비교하여 방향을 설정합니다.
+        if (playerInfo.x > otherPlayer.x) {
+          otherPlayer.flipX = true;
+        } else if (playerInfo.x < otherPlayer.x) {
+          otherPlayer.flipX = false;
         }
 
-        this.m_background.setX(this.m_player.x - Config.width / 2);
-        this.m_background.setY(this.m_player.y - Config.height / 2);
+        // 이전 위치를 업데이트합니다.
+        otherPlayer.previousX = otherPlayer.x;
+        otherPlayer.previousY = otherPlayer.y;
 
-        this.m_background.tilePositionX = this.m_player.x - Config.width / 2;
-        this.m_background.tilePositionY = this.m_player.y - Config.height / 2;
-    }
-
-    createClaw() {
-        // 플레이어의 위치에서 일정 거리(offset)만큼 앞으로 이동시킨 위치에서 Claw를 생성합니다.
-        const offset = -40; // 예시로 30 픽셀 앞으로 이동시킵니다.
-        const clawX = this.m_player.x + (this.m_player.flipX ? -offset : offset);
-        const clawY = this.m_player.y;
-
-        // Claw를 생성합니다. 플레이어의 위치, 방향 등을 적절히 설정하여 생성합니다.
-        const claw = new Claw(this, [clawX, clawY], this.m_player.flipX, 10, 1);
-        
-        // 플레이어의 움직임 벡터를 전달하여 Claw도 움직이도록 합니다.
-        const vector = [this.m_player.flipX ? -1 : 1, 0]; // 플레이어의 방향에 따라 Claw의 방향 설정
-        claw.move(vector);
-    }
-    
-
-    createPlayer() {
-        const x = Math.floor(Math.random() * 700) + 50;
-        const y = Math.floor(Math.random() * 500) + 50;
-        this.m_player = new Player(this, x, y, "player");
-        this.cameras.main.startFollow(this.m_player);
-    
-        // 플레이어 정보를 서버로 전송
-        this.socket.emit("joinRoom", { room: 0, x: this.m_player.x, y: this.m_player.y });
+        otherPlayer.setPosition(playerInfo.x, playerInfo.y);
       }
+    });
 
-      movePlayerManager() {
-        if (
-          this.m_cursorKeys.left.isDown ||
-          this.m_cursorKeys.right.isDown ||
-          this.m_cursorKeys.up.isDown ||
-          this.m_cursorKeys.down.isDown
-        ) {
-          if (!this.m_player.m_moving) {
-            this.m_player.play("player_anim");
-          }
-          this.m_player.m_moving = true;
-        } else {
-          if (this.m_player.m_moving) {
-            this.m_player.play("player_idle");
-          }
-          this.m_player.m_moving = false;
-        }
-    
-        // vector를 사용해 움직임을 관리할 것입니다.
-        // vector = [x좌표 방향, y좌표 방향]입니다.
-        // 왼쪽 키가 눌려있을 때는 vector[0] += -1, 오른쪽 키가 눌려있을 때는 vector[0] += 1을 해줍니다.
-        // 위/아래 또한 같은 방법으로 벡터를 수정해줍니다.
-        let vector = [0, 0];
-        if (this.m_cursorKeys.left.isDown) {
-          // player.x -= PLAYER_SPEED // 공개영상에서 진행했던 것
-          vector[0] += -1;
-        } else if (this.m_cursorKeys.right.isDown) {
-          vector[0] += 1;
-        }
-    
-        if (this.m_cursorKeys.up.isDown) {
-          vector[1] += -1;
-        } else if (this.m_cursorKeys.down.isDown) {
-          vector[1] += 1;
-        }
-    
-        if (vector[0] !== 0 || vector[1] !== 0) {
-          this.m_player.move(vector);
-          this.socket.emit("playerMovement", {
-            x: this.m_player.x,
-            y: this.m_player.y,
-          });
-        }
+    this.socket.on("disconnected", (playerId) => {
+      if (this.otherPlayers[playerId]) {
+        this.otherPlayers[playerId].destroy();
+        delete this.otherPlayers[playerId];
       }
-    
-      addOtherPlayers(playerInfo) {
-        const otherPlayer = new Player(this, playerInfo.x, playerInfo.y, "player");
-        otherPlayer.playerId = playerInfo.playerId;
-        this.otherPlayers[playerInfo.playerId] = otherPlayer;
-      }
+    });
+
+    // 사용할 sound들을 추가해놓는 부분입니다.
+    // load는 전역적으로 어떤 scene에서든 asset을 사용할 수 있도록 load 해주는 것이고,
+    // add는 해당 scene에서 사용할 수 있도록 scene의 멤버 변수로 추가할 때 사용하는 것입니다.
+    this.sound.pauseOnBlur = false;
+    this.m_beamSound = this.sound.add("audio_beam");
+    this.m_scratchSound = this.sound.add("audio_scratch");
+    this.m_hitMobSound = this.sound.add("audio_hitMob");
+    this.m_growlSound = this.sound.add("audio_growl");
+    this.m_explosionSound = this.sound.add("audio_explosion");
+    this.m_expUpSound = this.sound.add("audio_expUp");
+    this.m_hurtSound = this.sound.add("audio_hurt");
+    this.m_nextLevelSound = this.sound.add("audio_nextLevel");
+    this.m_gameOverSound = this.sound.add("audio_gameOver");
+    this.m_gameClearSound = this.sound.add("audio_gameClear");
+    this.m_pauseInSound = this.sound.add("audio_pauseIn");
+    this.m_pauseOutSound = this.sound.add("audio_pauseOut");
+
+    setBackground(this, "background1");
+
+    this.m_cursorKeys = this.input.keyboard.createCursorKeys();
+    this.m_attackKey = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.Z
+    );
+
+    this.createPlayer();
+  }
+
+  update() {
+    this.movePlayerManager();
+
+    if (Phaser.Input.Keyboard.JustDown(this.m_attackKey)) {
+      this.createClaw();
+      this.m_player.attack();
     }
-    
+
+    this.m_background.setX(this.m_player.x - Config.width / 2);
+    this.m_background.setY(this.m_player.y - Config.height / 2);
+
+    this.m_background.tilePositionX = this.m_player.x - Config.width / 2;
+    this.m_background.tilePositionY = this.m_player.y - Config.height / 2;
+  }
+
+  createClaw() {
+    // 플레이어의 위치에서 일정 거리(offset)만큼 앞으로 이동시킨 위치에서 Claw를 생성합니다.
+    const offset = -40; // 예시로 30 픽셀 앞으로 이동시킵니다.
+    const clawX = this.m_player.x + (this.m_player.flipX ? -offset : offset);
+    const clawY = this.m_player.y;
+
+    // Claw를 생성합니다. 플레이어의 위치, 방향 등을 적절히 설정하여 생성합니다.
+    const claw = new Claw(this, [clawX, clawY], this.m_player.flipX, 10, 1);
+
+    // 플레이어의 움직임 벡터를 전달하여 Claw도 움직이도록 합니다.
+    const vector = [this.m_player.flipX ? -1 : 1, 0]; // 플레이어의 방향에 따라 Claw의 방향 설정
+    claw.move(vector);
+  }
+
+  createPlayer() {
+    const x = Math.floor(Math.random() * 700) + 50;
+    const y = Math.floor(Math.random() * 500) + 50;
+    this.m_player = new Player(this, x, y, "player");
+    this.m_player.play("player_idle");
+    this.cameras.main.startFollow(this.m_player);
+
+    // 플레이어 정보를 서버로 전송
+    this.socket.emit("joinRoom", {
+      room: 0,
+      x: this.m_player.x,
+      y: this.m_player.y,
+    });
+  }
+
+  movePlayerManager() {
+    if (
+      this.m_cursorKeys.left.isDown ||
+      this.m_cursorKeys.right.isDown ||
+      this.m_cursorKeys.up.isDown ||
+      this.m_cursorKeys.down.isDown
+    ) {
+      if (!this.m_player.m_moving) {
+        this.m_player.play("player_anim");
+      }
+      this.m_player.m_moving = true;
+    } else {
+      if (this.m_player.m_moving) {
+        this.m_player.play("player_idle");
+      }
+      this.m_player.m_moving = false;
+    }
+
+    // vector를 사용해 움직임을 관리할 것입니다.
+    // vector = [x좌표 방향, y좌표 방향]입니다.
+    // 왼쪽 키가 눌려있을 때는 vector[0] += -1, 오른쪽 키가 눌려있을 때는 vector[0] += 1을 해줍니다.
+    // 위/아래 또한 같은 방법으로 벡터를 수정해줍니다.
+    let vector = [0, 0];
+    if (this.m_cursorKeys.left.isDown) {
+      // player.x -= PLAYER_SPEED // 공개영상에서 진행했던 것
+      vector[0] += -1;
+    } else if (this.m_cursorKeys.right.isDown) {
+      vector[0] += 1;
+    }
+
+    if (this.m_cursorKeys.up.isDown) {
+      vector[1] += -1;
+    } else if (this.m_cursorKeys.down.isDown) {
+      vector[1] += 1;
+    }
+
+    if (vector[0] !== 0 || vector[1] !== 0) {
+      this.m_player.move(vector);
+      this.socket.emit("playerMovement", {
+        x: this.m_player.x,
+        y: this.m_player.y,
+      });
+    }
+  }
+
+  addOtherPlayers(playerInfo) {
+    const otherPlayer = new Player(this, playerInfo.x, playerInfo.y, "player");
+    otherPlayer.playerId = playerInfo.playerId;
+    this.otherPlayers[playerInfo.playerId] = otherPlayer;
+    otherPlayer.play("player_idle");
+  }
+}
