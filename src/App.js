84f41c71b1ab7e57e1bdcebd69dd4b4452e55078
jh9193 <162890912+jh9193@users.noreply.github.com>
2024-06-18 15:18:38 +0900

    React Phaser template upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import Phaser from 'phaser';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Config from "./Config";
 
-export default App;
+const game = new Phaser.Game(Config);
+
+export default game;
