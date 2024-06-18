31f2d600cc7c2e48ed69644d740383b2ad514afd
Yoosun-Chang <12201321@inha.edu>
2024-06-18 19:10:56 +0900

    Feat: playerMovement 움직임이 있을때만 전송하도록 수정

--- src/characters/Player.js
+++ src/characters/Player.js
@@ -25,18 +25,23 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
   move(vector) {
     if (this.m_attacking || !this.m_canMove) return;
     let PLAYER_SPEED = 3;
-
+  
+    const previousPosition = { x: this.x, y: this.y };
+  
     this.x += vector[0] * PLAYER_SPEED;
     this.y += vector[1] * PLAYER_SPEED;
-
+  
     if (vector[0] === -1) this.flipX = false;
     else if (vector[0] === 1) this.flipX = true;
-
-    this.scene.socket.emit("playerMovement", {
-      x: this.scene.m_player.x,
-      y: this.scene.m_player.y,
-    });
+  
+    if (this.x !== previousPosition.x || this.y !== previousPosition.y) {
+      this.scene.socket.emit("playerMovement", {
+        x: this.x,
+        y: this.y,
+      });
+    }
   }
+
 
   attack() {
     // if (this.m_attacking) return; // 이미 공격 중이면 아무것도 하지 않음
