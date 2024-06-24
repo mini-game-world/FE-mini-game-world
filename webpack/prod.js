94d1922c6724b070231f14e86a8e90b7f0a40a09
최준성 <147041315+yumXD@users.noreply.github.com>
2024-06-24 10:46:29 +0900

    Feat: 빌드 관련 파일 수정

--- webpack/prod.js
+++ webpack/prod.js
@@ -6,22 +6,39 @@ const TerserPlugin = require("terser-webpack-plugin");
 module.exports = merge(base, {
   mode: "production",
   output: {
-    filename: "bundle.min.js"
+    filename: "bundle.min.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg|ogg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+              outputPath: "assets/",
+            },
+          },
+        ],
+      },
+    ],
   },
   devtool: false,
   performance: {
     maxEntrypointSize: 900000,
-    maxAssetSize: 900000
+    maxAssetSize: 900000,
   },
   optimization: {
     minimizer: [
       new TerserPlugin({
         terserOptions: {
           output: {
-            comments: false
-          }
-        }
-      })
-    ]
-  }
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
 });
