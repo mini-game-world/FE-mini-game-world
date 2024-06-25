8bdd2bd04b67912d7bf2c74eabbb78f8e30178b3
jh9193 <162890912+jh9193@users.noreply.github.com>
2024-06-25 23:53:16 +0900

    Feat: BGM리스트에서 Random재생 구현
    Co-authored-by: Yoosun-Chang <12201321@inha.edu>
    Co-authored-by: yumXD <qkpw99@gmail.com>

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -12,7 +12,9 @@ class BootScene extends Phaser.Scene {
     this.load.audio("explosion_sound", "/sounds/explosion.ogg");
     this.load.audio("timer_sound", "/sounds/timer.ogg");
     this.load.audio('playingBGM1', '/bgm/playingBGM1.mp3');
+    this.load.audio('playingBGM2', '/bgm/playingBGM2.mp3');
     this.load.audio('waitingBGM1', '/bgm/waitingBGM1.mp3');
+    this.load.audio('waitingBGM2', '/bgm/waitingBGM2.mp3');
     this.load.tilemapTiledJSON('map', '/maps/testMap1.tmj');
     this.load.image('tiles', '/tiles/first_tileset.png')
 
