caa68b448438276391f2a61009ccde22e92a406b
최준성 <147041315+yumXD@users.noreply.github.com>
2024-06-26 16:01:18 +0900

    Fix: 서버에 올리기 위해 수정...

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,67 +1,84 @@
 import Phaser from "phaser";
 
+import playerDeadSprite from "../assets/playerDead.png";
+import clawSprite from "../assets/claw_white.png";
+import bombSprite from "../assets/bomb.png";
+
+import testMap1 from "../assets/maps/testMap1.tmj";
+import first_tileset from "../assets/tiles/first_tileset.png";
+
+import scratchSound from "../assets/sounds/scratch.ogg";
+import explosionSound from "../assets/sounds/explosion.ogg";
+import timerSound from "../assets/sounds/timer.ogg";
+import winnerSound from "../assets/sounds/winner.ogg";
+
+import playingBGM1 from "../assets/bgm/playingBGM1.mp3";
+import playingBGM2 from "../assets/bgm/playingBGM2.mp3";
+import waitingBGM1 from "../assets/bgm/waitingBGM1.mp3";
+import waitingBGM2 from "../assets/bgm/waitingBGM2.mp3";
+
+const playerAssets = [];
+for (let i = 0; i <= 30; i++) {
+  playerAssets.push({
+    idle: require(`../assets/players/player${i}.png`),
+    move: require(`../assets/players/player_move${i}.png`),
+    attack: require(`../assets/players/player_attack${i}.png`),
+    stun: require(`../assets/players/player_stun${i}.png`),
+  });
+}
+
 class BootScene extends Phaser.Scene {
   constructor() {
     super("BootScene");
   }
 
   preload() {
-    this.load.setPath("src/assets");
-    this.load.image("background", "background.png");
-    this.load.audio("scratch_sound", "/sounds/scratch.ogg");
-    this.load.audio("explosion_sound", "/sounds/explosion.ogg");
-    this.load.audio("timer_sound", "/sounds/timer.ogg");
-    this.load.audio("winner_sound", "/sounds/winner.ogg");
-    this.load.audio("playingBGM1", "/bgm/playingBGM1.mp3");
-    this.load.audio("playingBGM2", "/bgm/playingBGM2.mp3");
-    this.load.audio("waitingBGM1", "/bgm/waitingBGM1.mp3");
-    this.load.audio("waitingBGM2", "/bgm/waitingBGM2.mp3");
-    this.load.tilemapTiledJSON("map", "/maps/testMap1.tmj");
-    this.load.image("tiles", "/tiles/first_tileset.png");
+    this.load.audio("scratch_sound", scratchSound);
+    this.load.audio("explosion_sound", explosionSound);
+    this.load.audio("timer_sound", timerSound);
+    this.load.audio("winner_sound", winnerSound);
 
-    for (let i = 0; i < 31; i++) {
-      this.load.spritesheet(`player${i}`, `/players/player${i}.png`, {
-        frameWidth: 200,
-        frameHeight: 220,
-      });
-      this.load.spritesheet(`player_move${i}`, `/players/player_move${i}.png`, {
-        frameWidth: 200,
-        frameHeight: 220,
-      });
-      this.load.spritesheet(
-        `player_attack${i}`,
-        `/players/player_attack${i}.png`,
-        {
-          frameWidth: 200,
-          frameHeight: 220,
-        }
-      );
-      this.load.spritesheet(`player_stun${i}`, `/players/player_stun${i}.png`, {
-        frameWidth: 200,
-        frameHeight: 220,
-      });
-    }
-    this.load.spritesheet("playerDead", "playerDead.png", {
+    this.load.audio("playingBGM1", playingBGM1);
+    this.load.audio("playingBGM2", playingBGM2);
+    this.load.audio("waitingBGM1", waitingBGM1);
+    this.load.audio("waitingBGM2", waitingBGM2);
+
+    this.load.tilemapTiledJSON("map", testMap1);
+    this.load.image("tiles", first_tileset);
+
+    this.load.spritesheet("playerDead", playerDeadSprite, {
       frameWidth: 150,
       frameHeight: 150,
     });
 
-    this.load.audio("audio_scratch", "/sounds/scratch.ogg");
-
-    this.load.spritesheet("claw_white", "claw_white.png", {
+    this.load.spritesheet("claw_white", clawSprite, {
       frameWidth: 32,
       frameHeight: 32,
     });
 
-    this.load.spritesheet("explosion", "explosion.png", {
-      frameWidth: 32,
-      frameHeight: 32,
-    });
-
-    this.load.spritesheet("bomb", "bomb.png", {
+    this.load.spritesheet("bomb", bombSprite, {
       frameWidth: 303,
       frameHeight: 142,
     });
+
+    for (let i = 0; i <= 30; i++) {
+      this.load.spritesheet(`player${i}`, playerAssets[i].idle, {
+        frameWidth: 200,
+        frameHeight: 220,
+      });
+      this.load.spritesheet(`player_move${i}`, playerAssets[i].move, {
+        frameWidth: 200,
+        frameHeight: 220,
+      });
+      this.load.spritesheet(`player_attack${i}`, playerAssets[i].attack, {
+        frameWidth: 200,
+        frameHeight: 220,
+      });
+      this.load.spritesheet(`player_stun${i}`, playerAssets[i].stun, {
+        frameWidth: 200,
+        frameHeight: 220,
+      });
+    }
   }
 
   create() {
