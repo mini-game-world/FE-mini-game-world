10bf012806de4145fbf78615ad17c1caaeb5fdb0
최준성 <147041315+yumXD@users.noreply.github.com>
2024-07-03 16:01:25 +0900

    Refactor: 예외 확인 추가

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -148,7 +148,9 @@ class GameScene extends Phaser.Scene {
     });
 
     SocketManager.onAttackPlayer((id) => {
-      this.players[id].createClawAttack();
+      if (this.players[id]) {
+        this.players[id].createClawAttack();
+      }
     });
 
     SocketManager.onPlayerDisconnected((id) => {
@@ -196,7 +198,9 @@ class GameScene extends Phaser.Scene {
 
     SocketManager.onBombUsers((players) => {
       players.forEach((id) => {
-        this.players[id].setBombUser();
+        if (this.players[id]) {
+          this.players[id].setBombUser();
+        }
       });
     });
 
