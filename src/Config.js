0c0251d4e5a71e35074c48fc410e117e418c7e31
최준성 <147041315+yumXD@users.noreply.github.com>
2024-07-08 21:13:05 +0900

    Feat: 모바일 감지 추가

--- src/Config.js
+++ src/Config.js
@@ -4,6 +4,9 @@ import MainScene from "./scenes/MainScene";
 import GameScene from "./scenes/GameScene";
 import TutorialScene from "./scenes/TutorialScene";
 
+// 모바일 기기 감지
+const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+
 const Config = {
   type: Phaser.AUTO,
   width: 3840,
@@ -13,6 +16,7 @@ const Config = {
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
+    orientation: Phaser.Scale.LANDSCAPE,
   },
   physics: {
     default: "arcade",
@@ -20,6 +24,11 @@ const Config = {
       debug: false,
     },
   },
+  parent: "game-container",
 };
+
+if (isMobile) {
+  Config.scale.orientation = Phaser.Scale.LANDSCAPE;
+}
 
 export default Config;
