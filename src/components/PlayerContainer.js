7c1ba9485f7d214cf42b948ab12be848268dab77
최준성 <147041315+yumXD@users.noreply.github.com>
2024-07-09 20:04:05 +0900

    Refactor: 폭탄 터지는 위치 수정

--- src/components/PlayerContainer.js
+++ src/components/PlayerContainer.js
@@ -268,9 +268,6 @@ class PlayerContainer extends Phaser.GameObjects.Container {
       if (this.arrow) {
         this.arrow.updatePosition(); // Arrow 위치 업데이트
       }
-      if (this.bomb) {
-        this.bomb.updatePosition();
-      }
 
       // 컨테이너 위치 업데이트
       this.setPosition(this.hitBox.x, this.hitBox.y);
