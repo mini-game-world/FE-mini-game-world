8bc036d72d76a71d635ef4b5ffded4fb003419a7
최준성 <147041315+yumXD@users.noreply.github.com>
2024-07-12 18:57:43 +0900

    Fix: 캐릭터 중복 생성되는 버그 있음 막음;;

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -51,24 +51,28 @@ class GameScene extends Phaser.Scene {
         const { x, y, avatar, isPlay, isDead, nickname } = players[id];
         const isSelfInitiated = id === SocketManager.channel.id;
         const info = { avatar, isPlay, isDead, nickname, isSelfInitiated };
-        const playerContainer = new PlayerContainer(
-          this,
-          x,
-          y,
-          `player${avatar}`,
-          info
-        );
-        this.players[id] = playerContainer;
 
-        if (isSelfInitiated) {
-          this.player = playerContainer;
-          this.cameraManager.smoothFollow(this.player);
-          this.physics.add.collider(this.player.hitBox, this.backGround);
-          this.physics.add.collider(this.player.hitBox, this.fence);
-          this.physics.add.collider(this.player.hitBox, this.house);
-          this.physics.add.collider(this.player.hitBox, this.object);
+        // 이미 플레이어 객체가 존재하면 무시
+        if (!this.players[id]) {
+          const playerContainer = new PlayerContainer(
+            this,
+            x,
+            y,
+            `player${avatar}`,
+            info
+          );
+          this.players[id] = playerContainer;
 
-          this.mapShrinker.applyPreviousShrinks();
+          if (!this.player && isSelfInitiated) {
+            this.player = playerContainer;
+            this.cameraManager.smoothFollow(this.player);
+            this.physics.add.collider(this.player.hitBox, this.backGround);
+            this.physics.add.collider(this.player.hitBox, this.fence);
+            this.physics.add.collider(this.player.hitBox, this.house);
+            this.physics.add.collider(this.player.hitBox, this.object);
+
+            this.mapShrinker.applyPreviousShrinks();
+          }
         }
       });
       this.updatePlayerCountText();
@@ -78,16 +82,20 @@ class GameScene extends Phaser.Scene {
       const { playerId, x, y, avatar, nickname } = player;
       const isSelfInitiated = false;
       const info = { avatar, nickname, isSelfInitiated };
-      const newPlayer = new PlayerContainer(
-        this,
-        x,
-        y,
-        `player${avatar}`,
-        info
-      );
-      this.players[playerId] = newPlayer;
 
-      this.updatePlayerCountText();
+      // 이미 플레이어 객체가 존재하면 무시
+      if (!this.players[playerId]) {
+        const newPlayer = new PlayerContainer(
+          this,
+          x,
+          y,
+          `player${avatar}`,
+          info
+        );
+        this.players[playerId] = newPlayer;
+
+        this.updatePlayerCountText();
+      }
     });
 
     SocketManager.onPlayerMoved((player) => {
