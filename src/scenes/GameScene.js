3eb0e2481a66a63719ffe4a2dd5300f9ef2b6855
최준성 <147041315+yumXD@users.noreply.github.com>
2024-07-13 16:58:30 +0900

    Fix: 게임 결과 텍스트 안사라지는 버그 수정

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -149,6 +149,9 @@ class GameScene extends Phaser.Scene {
           this.bgmManager.playWaitingRandomBGM();
         }
         this.gameStatusText.showEnd();
+        if (this.resultText) {
+          this.resultText.del();
+        }
 
         Object.values(this.players).forEach((player) => {
           if (this.player === player) {
