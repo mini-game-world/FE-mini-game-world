ec41833d86727747d8389932fd96623037296df8
최준성 <147041315+yumXD@users.noreply.github.com>
2024-07-13 15:31:43 +0900

    Refactor: 백그라운드 관련 로직 모듈화

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -10,6 +10,7 @@ import Item from "../components/Item";
 import ItemEffect from "../components/ItemEffect";
 import RankText from "../components/RankText";
 import InfoText from "../components/InfoText";
+import BackgroundManager from "../utils/BackgroundManager";
 import "regenerator-runtime/runtime";
 
 class GameScene extends Phaser.Scene {
@@ -27,13 +28,14 @@ class GameScene extends Phaser.Scene {
     this.cameraManager = null;
     this.mapShrinker = null;
     this.bgmManager = null;
+    this.backgroundManager = null;
 
     this.itemsGroup = null;
   }
 
   create() {
     SocketManager.connect();
-    this.setBackground();
+    this.backgroundManager = new BackgroundManager(this);
     this.cameraManager = new CameraManager(this);
 
     this.resultText = new ResultText(this);
@@ -338,70 +340,6 @@ class GameScene extends Phaser.Scene {
         );
       }
     });
-  }
-  setBackground() {
-    // 타일맵 설정
-    const map = this.make.tilemap({ key: "map" });
-    const house_1 = map.addTilesetImage("house_1", "house_1");
-    const logs = map.addTilesetImage("logs", "logs");
-    const stump_2 = map.addTilesetImage("stump_2", "stump_2");
-    const Tileset_1 = map.addTilesetImage("Tileset_1", "Tileset_1");
-    const tree_1 = map.addTilesetImage("tree_1", "tree_1");
-    const tree_2 = map.addTilesetImage("tree_2", "tree_2");
-    const stone_1 = map.addTilesetImage("stone_1", "stone_1");
-    const stone_3 = map.addTilesetImage("stone_3", "stone_3");
-    const fence_1 = map.addTilesetImage("fence_1", "fence_1");
-    const fence_3 = map.addTilesetImage("fence_3", "fence_3");
-
-    // 레이어 생성 (Tiled에서 설정한 레이어 이름 사용)
-
-    this.backGround = map.createLayer("BackGround", Tileset_1, 0, 0);
-    this.backGround.setCollisionByProperty({ collides: true });
-    this.fence = map.createLayer("Fence", [fence_1, fence_3], 0, 0);
-    this.fence.setCollisionByProperty({ collides: true });
-    this.house = map.createLayer("House", house_1, 0, 0);
-    this.house.setCollisionByProperty({ collides: true });
-    this.object = map.createLayer(
-      "Object",
-      [Tileset_1, logs, stump_2, tree_1, tree_2, stone_1, stone_3],
-      0,
-      0
-    );
-    this.object.setCollisionByProperty({ collides: true });
-
-    this.mapShrink = map.createLayer("MapShrink", Tileset_1, 0, 0);
-    this.mapShrink.setCollisionByProperty({ collides: true });
-
-    // 충돌 디버그 그래픽 추가
-    // this.debugGraphics = this.add.graphics();
-    // this.backGround.renderDebug(this.debugGraphics, {
-    //   tileColor: null, // 충돌하지 않는 타일은 표시하지 않음
-    //   collidingTileColor: new Phaser.Display.Color(255, 0, 0, 128), // 충돌 타일은 반투명 빨간색으로 표시
-    //   faceColor: new Phaser.Display.Color(0, 255, 0, 128), // 충돌하는 면은 반투명 녹색으로 표시
-    // });
-    // this.house.renderDebug(this.debugGraphics, {
-    //   tileColor: null, // 충돌하지 않는 타일은 표시하지 않음
-    //   collidingTileColor: new Phaser.Display.Color(255, 0, 0, 128), // 충돌 타일은 반투명 빨간색으로 표시
-    //   faceColor: new Phaser.Display.Color(0, 255, 0, 128), // 충돌하는 면은 반투명 녹색으로 표시
-    // });
-    // this.object.renderDebug(this.debugGraphics, {
-    //   tileColor: null, // 충돌하지 않는 타일은 표시하지 않음
-    //   collidingTileColor: new Phaser.Display.Color(255, 0, 0, 128), // 충돌 타일은 반투명 빨간색으로 표시
-    //   faceColor: new Phaser.Display.Color(0, 255, 0, 128), // 충돌하는 면은 반투명 녹색으로 표시
-    // });
-    // this.fence.renderDebug(this.debugGraphics, {
-    //   tileColor: null, // 충돌하지 않는 타일은 표시하지 않음
-    //   collidingTileColor: new Phaser.Display.Color(255, 0, 0, 128), // 충돌 타일은 반투명 빨간색으로 표시
-    //   faceColor: new Phaser.Display.Color(0, 255, 0, 128), // 충돌하는 면은 반투명 녹색으로 표시
-    // });
-    // this.mapShrink.renderDebug(this.debugGraphics, {
-    //   tileColor: null, // 충돌하지 않는 타일은 표시하지 않음
-    //   collidingTileColor: new Phaser.Display.Color(255, 0, 0, 128), // 충돌 타일은 반투명 빨간색으로 표시
-    //   faceColor: new Phaser.Display.Color(0, 255, 0, 128), // 충돌하는 면은 반투명 녹색으로 표시
-    // });
-
-    // Set world bounds
-    this.physics.world.setBounds(0, 0, 4160, 3200);
   }
 
   updatePlayerCountText() {
